0c2b9d424aba828262c4590353f413b0c9727c85
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-07-29 19:15:10 +0100

    Code cleanup, moved config struct to the top, removed unneccessary commented out code.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,22 @@ use std::io::prelude::*;
 use std::process;
 use std::mem;
 
+struct Config {
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments.\nYou must specify a filename to check.");
+        }
+        else if args.len() > 2 {
+            return Err("Can only check one file at a time.")
+        }
+        let filename = args[1].clone();
+        Ok(Config {filename})
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,21 +34,6 @@ fn main() {
     // Reading in a file
     let mut f = File::open(config.filename).expect("File Not Found :(");
 
-    // let path = std::path::Path::new(&config.filename);
-    // let file = std::fs::File::create(&path).unwrap();
-
-    // let mut zip = zip::ZipWriter::new(file);
-
-    // try!(zip.add_directory("test/", FileOptions::default()));
-
-    // let options = FileOptions::default().compression_method(zip::CompressionMethod::Stored).unix_permissions(0o755);
-    // try!(zip.start_file("test/☃.txt", options));
-    // try!(zip.write_all(b"Hello, World!\n"));
-
-    // try!(zip.start_file("test/lorem_ipsum.txt", FileOptions::default()));
-
-    // try!(zip.finish());
-    
     let mut contents = String::new();
     &f.read_to_string(&mut contents)
     .expect("Something went wrong :( Could not read the file");
@@ -44,29 +45,21 @@ fn main() {
 
     let word_vec = assemble_word_vec(&word_file_contents);
 
-    for line in search(&config.query, &contents, &word_vec) {
+    for line in search(&contents, &word_vec) {
         println!("{}", line);
     }
 
     // println!("Contains:\n{}", contents);
 }
 
-pub fn search<'a>(query: &str, contents: &'a str, word_vec : &Vec<&str>) -> Vec<&'a str> {
+pub fn search<'a>(contents: &'a str, word_vec : &Vec<&str>) -> Vec<&'a str> {
     let dict = vec!["dreary", "Who", "how", "somebody"];
-    // let alphabet = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     let mut results = Vec::new();
 
     for line in contents.lines() {
         let split_line = line.split(" ");
         let vec = split_line.collect::<Vec<&str>>();
-        // for item in &vec {
-        //     let mut item = item.to_lowercase();
-        //     println!("{}", item);
-        // }
-
-        println!("{:?}", vec);
-
 
         for item in &vec {
             // TODO strip out chars
@@ -86,7 +79,6 @@ pub fn search<'a>(query: &str, contents: &'a str, word_vec : &Vec<&str>) -> Vec<
                 for c in item.chars() {
                 if c.is_alphabetic() {
                         stripped_word.push(c);
-                    
                 }
                 else {
                     continue
@@ -135,22 +127,3 @@ pub fn assemble_word_vec<'a>(contents: &'a str) -> Vec<&'a str> {
     word_vec
 }
 
-struct Config {
-    filename: String,
-    query : String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.\nYou must specify a filename to check.");
-        }
-        else if args.len() > 3 {
-            return Err("Can only check one file at a time.")
-        }
-        let filename = args[2].clone();
-        let query = args[1].clone();
-
-        Ok(Config {query, filename})
-    }
-}
